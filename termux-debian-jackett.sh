fc00ddfe5db3f18f1f4f8f09e57d2785847fb0b0
d.maznekov <d.maznekov@gmail.com>
2022-10-31 10:01:16 +0200

    Bugfix

--- termux-debian-jackett.sh
+++ termux-debian-jackett.sh
@@ -1,28 +1,25 @@
-#!/bin/bash
-#pkg update
-#pkg upgrade -y
-#pkg install proot-distro wget 
-#apt autoremove -y
+#!/bin/sh
+pkg update
+pkg upgrade -y
+pkg install proot-distro wget 
+apt autoremove
 ver=$( curl -s -H 'Pragma: no-cache' "https://api.github.com/repos/Jackett/Jackett/releases/latest" | awk -F '"' '/tag_name/{print $4}' | tr -d '[:cntrl:]' )
-echo "Latest Jackett version $ver"
-echo "https://github.com/Jackett/Jackett/releases/download/${ver}/Jackett.Binaries.LinuxAMDx64.tar.gz"
+echo ">>> Latest Jackett version $ver"
 
-ARCH=$( uname -m )
-echo "$ARCH"
-case "$ARCH" in
-  ( "x86_64" ) 
-	wget https://github.com/Jackett/Jackett/releases/download/${ver}/Jackett.Binaries.LinuxAMDx64.tar.gz -O /sdcard/Download/Jackett.Binaries.tar.gz
+case $( uname -m | tr '[:upper:]' '[:lower:]') in
+	"x86_64" ) 
+		targetos=LinuxAMD;;
+	"aarch64" )
+		targetos=LinuxARM64;;
+    "aarch32"|"armv7*" ) 
+		targetos=LinuxARM32;;
+	* )
+		echo "Unsupported OS, exiting..."
+		exit 1
 	;;
-  ( "aarch64" ) 
-	wget https://github.com/Jackett/Jackett/releases/download/${ver}/Jackett.Binaries.LinuxARM64.tar.gz -O /sdcard/Download/Jackett.Binaries.tar.gz
-	;;
-  ( "aarch32"|"armv7*" ) 
-	wget https://github.com/Jackett/Jackett/releases/download/${ver}/Jackett.Binaries.LinuxARM32.tar.gz -O /sdcard/Download/Jackett.Binaries.tar.gz
-	;;
-  *) 
-	echo "Unknow architecture, exiting..."
-	exit 1
 esac
+URL=$( curl -s -H 'Pragma: no-cache' "https://api.github.com/repos/Jackett/Jackett/releases/latest" | grep browser_download_url | cut -d '"' -f 4 | grep ${targetos} )
+wget "$URL" -O /sdcard/Download/Jackett.Binaries.tar.gz
 #rm -rf /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/Jackett
 proot-distro install debian
 proot-distro login debian -- tar -xvzf /sdcard/Download/Jackett.Binaries.tar.gz
